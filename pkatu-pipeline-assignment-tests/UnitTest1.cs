ca75fe601461aa8c188eebc680478e356e492d8f
patrick.kearns <L00186459@atu.ie>
2023-11-22 16:36:30 +0000

    initial commit

---
+++ pkatu-pipeline-assignment-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace pkatu_pipeline_assignment_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
