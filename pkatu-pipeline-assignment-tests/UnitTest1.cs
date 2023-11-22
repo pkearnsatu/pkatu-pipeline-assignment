01819cbca5e6c63651f555ee13858e46caea9681
patrick.kearns <L00186459@atu.ie>
2023-11-22 18:47:40 +0000

    feat: add something to test on web page & working unit test for small method

--- pkatu-pipeline-assignment-tests/UnitTest1.cs
+++ pkatu-pipeline-assignment-tests/UnitTest1.cs
@@ -1,16 +1,28 @@
+using NUnit.Framework;
+using pkatu_pipeline_assignment;
+using pkatu_pipeline_assignment.Services;
+
 namespace pkatu_pipeline_assignment_tests
 {
     public class Tests
     {
+        private CalcService _calcService;
+
         [SetUp]
         public void Setup()
         {
+           
+            _calcService = new CalcService();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            // Call the method
+            double result = _calcService.GetSquareRoot(150);
+
+            // Assert that the method returns the expected number
+            Assert.That(result, Is.EqualTo(12.24744871391589), "The method did not return the expected number.");
         }
     }
 }
---
+++ pkatu-pipeline-assignment/Services/CalcService.cs
@@ -0,0 +1,10 @@
+﻿namespace pkatu_pipeline_assignment.Services
+{
+    public class CalcService
+    {
+        public double GetSquareRoot(int numberToGetSquare)
+        {
+            return Math.Sqrt(numberToGetSquare);
+        }
+    }
+}
